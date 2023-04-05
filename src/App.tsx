8983c21e06d1c6ee8734736dc07341ac9185bbc8
BrandenBoucher <bgbouche@asu.edu>
2023-04-05 14:34:16 -0700

    Finished for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,240 +1,210 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import { stringify } from 'querystring';
+import { useState } from 'react';
 import './App.css';
+import { noteBar, bars } from './notes';
 
-const notes = ["R", "F", "F#", "G", "G#",
-    "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#",
-    "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#",
-    "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#",
-    "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#",
-    "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G"];
+const middleCIndex = 32;
+
+type scriptEntry = {
+    name: string,
+    length: string,
+    id: number
+}
+
+const restBar: noteBar = {
+    noteName: 'R',
+    octave: 0,
+    lineType: 'none',
+    hasSharp: false
+}
 
 function App() {
 
-    const [script, setScript] = useState('Empty');
+    const [script, setScript] = useState<scriptEntry[]>([]);
     const [noteLen, setNoteLen] = useState('1/16');
+    const [scriptName, setScriptName] = useState('');
 
-    const pushToScript = (noteVal: number) => {
-        alert(noteVal);
-        alert(notes[noteVal]);
+    const pushToScript = (bar: noteBar, id: number) => {
+        console.log(bar.noteName + bar.octave + ":" + id);
+        const entry: scriptEntry = { name: bar.noteName + bar.octave, length: noteLen, id: id };
+        const tempScript = Array.from(script);
+        tempScript.push(entry);
+        setScript(tempScript);
+
     };
+
+    const removeLastScriptEntry = () => {
+        const tempScript = Array.from(script);
+        tempScript.pop();
+        setScript(tempScript);
+    }
+
+    const removeScriptEntry = (entry: scriptEntry) => {
+
+        const tempScript: scriptEntry[] = [];
+
+        script.forEach(element => {
+            if (element !== entry)
+                tempScript.push(element);
+        });
+
+        setScript(tempScript);
+    }
 
     const noteLenSelected = (length: string) => {
         setNoteLen(length);
-        alert(length);
     }
 
-    const getNote = (noteNumber: number) => {
+    const generateEncodedScripts = () => {
+        const noteScript = generateNoteScripts();
+        const extLenScript = generateLengthExtensionScript();
+        const noteText = generateNotesToText();
+        const blob = new Blob([noteScript + '\n', extLenScript + '\n',noteText], {type: 'text/plain'});
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.download = (scriptName ? scriptName + '-' : '') + 'notes.txt';
+        link.href = url;
+        link.click();
+
+        console.log(noteScript);
+        console.log(extLenScript);
+        console.log(noteText);
+    }
+
+    const generateNotesToText = () => {
+        let noteText: string = '';
+        script.forEach((element, index) => {
+            noteText += index + ": " + element.length + " " + element.name + "\n";
+        })
+
+        return noteText;
+    }
+
+    const generateNoteScripts = () => {
+        let noteScript: string = '{';
+        let noteVal: number;
+
+
+        script.forEach(element => {
+
+            switch (element.length) {
+
+                case '1/1':
+                    noteVal = 0;
+                    break;
+                case '1/2':
+                    noteVal = 48;
+                    break;
+                case '1/4':
+                    noteVal = 32;
+                    break;
+                case '1/8':
+                    noteVal = 16;
+                    break;
+                case '1/16':
+                default:
+                    noteVal = 0;
+                    break;
+            }
+
+
+
+            noteVal += element.id;
+
+            noteScript += "0x" + noteVal.toString(16) + ",";
+
+        })
+
+        return cleanEncodedScript(noteScript);
+    }
+
+    const generateLengthExtensionScript = () => {
+        let lenExtScript: string = '{';
+        let lengthExt: number = 0;
+        let bitPos: number = 0;
+
+        script.forEach(element => {
+
+            console.log("bit pos: " + bitPos);
+            if (element.length === '1/1') {
+                console.log("add " + Math.pow(2, bitPos));
+                lengthExt += Math.pow(2, bitPos);
+                console.log(lengthExt);
+            }
+
+            bitPos++;
+
+            if (bitPos >= 8) {
+                lenExtScript += "0x" + lengthExt.toString(16) + ",";
+                bitPos = 0;
+                lengthExt = 0;
+            }
+        });
+
+        //Get whatever is left
+        if (bitPos > 0) {
+            lenExtScript += "0x" + lengthExt.toString(16) + ",";
+            bitPos = 0;
+        }
+
+        return cleanEncodedScript(lenExtScript);
+
+    }
+
+    const cleanEncodedScript = (encodedScript: string) => {
+
+        if (encodedScript.endsWith(',')) {
+            encodedScript = encodedScript.slice(0, encodedScript.length - 1);
+        }
+
+        return encodedScript + '}';
+
+    }
+
+    const generateNotes = () => {
+        let id = 1;
+
         return (
-            <button onClick={() => pushToScript(noteNumber)}>{notes[noteNumber]}</button>
+            <div style={{ display: "flex", flexDirection: "column", flex: 0, marginLeft: "100px" }}> {
+                bars.map((bar: noteBar) => {
+                    const barComp = generateBar(bar, id);
+                    id += bar.hasSharp ? 2 : 1;
+                    return barComp;
+                })
+            }
+            </div>);
+    }
+
+    const generateBar = (bar: noteBar, id: number) => {
+
+        const className = bar.hasSharp ? 'note-and-sharp' : 'single-note';
+
+        return (
+            <>
+                {id === middleCIndex &&
+                    <div className="single-note">
+                        <hr style={{ height: "5px", background: "#FFFFFF", border: "none", }} />
+                    </div>
+                }
+                {bar.lineType === 'short' && <hr className='note-line-short' />}
+                {bar.lineType === 'long' && <hr className='note-line-long' />}
+                <div className={className}>
+                    {bar.hasSharp && <button onClick={() => pushToScript(bar, id)}>{bar.noteName}#{bar.octave}</button>}
+                    <button onClick={() => pushToScript(bar, id + (bar.hasSharp ? 1 : 0))}>{bar.noteName + bar.octave}</button>
+                </div>
+                {id === middleCIndex &&
+                    <div className="single-note">
+                        <hr style={{ height: "5px", background: "#FFFFFF", border: "none", }} />
+                    </div>
+                }
+            </>
         );
     }
 
     return (
         <div className="App" style={{ display: "flex", flexDirection: "row", marginTop: "10px" }}>
-            <div style={{ display: "flex", flexDirection: "column", flex: 0, marginLeft: "100px" }}>
-                {/* 63 */}
-                <hr className='note-line-short' />
-                <div className="single-note">
-                    {getNote(63)}
-                </div>
 
-                {/* 61, 62 */}
-                <div className="note-and-sharp">
-                    {getNote(62)}
-                    {getNote(61)}
-                </div>
-                {/* 60 */}
-                <hr className='note-line-short' />
-                <div className="single-note">
-                    {getNote(60)}
-                </div>
-                {/* 58, 59 */}
-                <div className="note-and-sharp">
-                    {getNote(59)}
-                    {getNote(58)}
-                </div>
-                <hr className='note-line-short' />
-                {/* 56, 57 */}
-                <div className="note-and-sharp">
-                    {getNote(57)}
-                    {getNote(56)}
-                </div>
-                {/* 55 */}
-                <div className="single-note">
-                    {getNote(55)}
-                </div>
-                <hr className='note-line-short' />
-                {/* 53, 54 */}
-                <div className="note-and-sharp">
-                    {getNote(54)}
-                    {getNote(53)}
-                </div>
-                {/* 51, 52 */}
-                <div className="note-and-sharp">
-                    {getNote(52)}
-                    {getNote(51)}
-                </div>
-                <hr className='note-line-long' />
-                {/* 49, 50 */}
-                <div className="note-and-sharp">
-                    {getNote(50)}
-                    {getNote(49)}
-                </div>
-                {/* 48 */}
-                <div className="single-note">
-                    {getNote(48)}
-                </div>
-                <hr className='note-line-long' />
-                {/* 46, 47*/}
-                <div className="note-and-sharp">
-                    {getNote(47)}
-                    {getNote(46)}
-                </div>
-                {/* 44, 45 */}
-                <div className="note-and-sharp">
-                    {getNote(45)}
-                    {getNote(44)}
-                </div>
-                {/* 43 */}
-                <hr className='note-line-long' />
-                <div className="single-note">
-                    {getNote(43)}
-                </div>
-                {/* 41, 42 */}
-                <div className="note-and-sharp">
-                    {getNote(42)}
-                    {getNote(41)}
-                </div>
-                <hr className='note-line-long' />
-                {/* 39, 40 */}
-                <div className="note-and-sharp">
-                    {getNote(40)}
-                    {getNote(39)}
-                </div>
-                {/* 37, 38 */}
-                <div className="note-and-sharp">
-                    {getNote(38)}
-                    {getNote(37)}
-                </div>
-                <hr className='note-line-long' />
-                {/* 36 */}
-                <div className="single-note">
-                    {getNote(36)}
-                </div>
-                {/* 34, 35 */}
-                <div className="note-and-sharp">
-                    {getNote(35)}
-                    {getNote(34)}
-                </div>
-                {/* 32, 33 */}
-                <div className="single-note">
-                    <hr style={{ height: "5px", background: "#FFFFFF", border: "none", }} />
-                </div>
-                <hr className='note-line-short' />
-                <div className="note-and-sharp">
-                    {getNote(33)}
-                    {getNote(32)}
-                </div>
-                <div>
-                    <hr style={{ height: "5px", background: "#FFFFFF", border: "none", }} />
-                </div>
-                <div></div>
-                {/* 31 */}
-                <div className="single-note">
-                    {getNote(31)}
-                </div>
-                <hr className='note-line-long' />
-                {/* 30, 29 */}
-                <div className="note-and-sharp">
-                    {getNote(30)}
-                    {getNote(29)}
-                </div>
-                {/* 28, 27 */}
-                <div className="note-and-sharp">
-                    {getNote(28)}
-                    {getNote(27)}
-                </div>
-                <hr className='note-line-long' />
-                {/* 26, 25 */}
-                <div className="note-and-sharp">
-                    {getNote(26)}
-                    {getNote(25)}
-                </div>
-                {/* 24 */}
-                <div className="single-note">
-                    {getNote(24)}
-                </div>
-                <hr className='note-line-long' />
-                {/* 23, 22 */}
-                <div className="note-and-sharp">
-                    {getNote(23)}
-                    {getNote(22)}
-                </div>
-                {/* 21, 20 */}
-                <div className="note-and-sharp">
-                    {getNote(21)}
-                    {getNote(20)}
-                </div>
-                <hr className='note-line-long' />
-                {/* 19 */}
-                <div className="single-note">
-                    {getNote(19)}
-                </div>
-                {/* 18, 17 */}
-                <div className="note-and-sharp">
-                    {getNote(18)}
-                    {getNote(17)}
-                </div>
-                <hr className='note-line-long' />
-                {/* 16, 15 */}
-                <div className="note-and-sharp">
-                    {getNote(16)}
-                    {getNote(15)}
-                </div>
-                {/* 14, 13 */}
-                <div className="note-and-sharp">
-                    {getNote(14)}
-                    {getNote(13)}
-                </div>
-                <hr className='note-line-short' />
-                {/* 12 */}
-                <div className="single-note">
-                    {getNote(12)}
-                </div>
-                {/* 11, 10 */}
-                <div className="note-and-sharp">
-                    {getNote(11)}
-                    {getNote(10)}
-                </div>
-                <hr className='note-line-short' />
-                {/* 9, 8 */}
-                <div className="note-and-sharp">
-                    {getNote(9)}
-                    {getNote(8)}
-                </div>
-                {/* 7 */}
-                <div className="single-note">
-                    {getNote(7)}
-                </div>
-                <hr className='note-line-short' />
-                {/* 6, 5 */}
-                <div className="note-and-sharp">
-                    {getNote(6)}
-                    {getNote(5)}
-                </div>
-                {/* 4, 3 */}
-                <div>
-                    {getNote(4)}
-                    {getNote(3)}
-                </div>
-                <hr className='note-line-short' />
-                {/* 2, 1 */}
-                <div >
-                    {getNote(2)}
-                    {getNote(1)}
-                </div>
-            </div>
+            <>{generateNotes()}</>
 
             <div style={{ flex: 0 }} >
                 <div style={{ display: "flex", flexDirection: "column", marginLeft: "20px", marginRight: "20px" }}>
@@ -244,13 +214,35 @@ function App() {
                     <div><input type="radio" value="1/4" name="length" onChange={() => noteLenSelected('1/4')} checked={noteLen === '1/4'} /> 1/4</div>
                     <div><input type="radio" value="1/2" name="length" onChange={() => noteLenSelected('1/2')} checked={noteLen === '1/2'} /> 1/2</div>
                     <div><input type="radio" value="1/1" name="length" onChange={() => noteLenSelected('1/1')} checked={noteLen === '1/1'} /> 1/1</div>
-                    <div><button style={{ width: "100px" }}>Add Rest</button></div>
-                    <div><button style={{ width: "100px" }}>Remove Last</button></div>
-                    <div><button style={{ width: "100px" }}>Export Script</button></div>
+                    <div><button style={{ width: "100px" }} onClick={() => pushToScript(restBar, 0)}>Add Rest</button></div>
+                    <div><button style={{ width: "100px" }} onClick={removeLastScriptEntry}>Remove Last</button></div>
+                    <div><button style={{ width: "100px" }} onClick={generateEncodedScripts}>Export Script</button></div>
                 </div>
             </div>
-            <div style={{ flex: 0 }} >
-                <textarea name="body">{script}</textarea>
+            <div style={{ flex: 0, minWidth: '140px' }} >
+                Script Name:
+                <input name='script-name' onChange={(event) => setScriptName(event.target.value)}/>
+                <text>Note Script:</text>
+                {
+                    script.map((entry: scriptEntry, index: number) => {
+                        return (
+                            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                                <div style={{ display: 'flex', flexDirection: 'row', width: '120px' }}>
+                                    <div style={{ width: '40px' }}>
+                                        {index}:
+                                    </div>
+                                    <div style={{ width: '40px' }}>
+                                        {entry.name}
+                                    </div>
+                                    <div>
+                                        {entry.length}
+                                    </div>
+                                </div>
+                                <button style={{ width: '20px' }} onClick={() => removeScriptEntry(entry)}>x</button>
+                            </div>
+                        );
+                    })
+                }
             </div>
         </div>
     );
---
+++ src/notes.ts
@@ -0,0 +1,231 @@
+export type noteBar = {
+  noteName: string;
+  hasSharp: boolean;
+  lineType: "none" | "short" | "long";
+  octave: number;
+};
+
+export const bars: noteBar[] = [
+  {
+    noteName: "G",
+    hasSharp: true,
+    lineType: "short",
+    octave: 6,
+  },
+  {
+    noteName: "F",
+    hasSharp: true,
+    lineType: "none",
+    octave: 6,
+  },
+  {
+    noteName: "E",
+    hasSharp: false,
+    lineType: "short",
+    octave: 6,
+  },
+  {
+    noteName: "D",
+    hasSharp: true,
+    lineType: "none",
+    octave: 6,
+  },
+  {
+    noteName: "C",
+    hasSharp: true,
+    lineType: "short",
+    octave: 6,
+  },
+  {
+    noteName: "B",
+    hasSharp: false,
+    lineType: "none",
+    octave: 5,
+  },
+  {
+    noteName: "A",
+    hasSharp: true,
+    lineType: "short",
+    octave: 5,
+  },
+  {
+    noteName: "G",
+    hasSharp: true,
+    lineType: "none",
+    octave: 5,
+  },
+  {
+    noteName: "F",
+    hasSharp: true,
+    lineType: "long",
+    octave: 5,
+  },
+  {
+    noteName: "E",
+    hasSharp: false,
+    lineType: "none",
+    octave: 5,
+  },
+  {
+    noteName: "D",
+    hasSharp: true,
+    lineType: "long",
+    octave: 5,
+  },
+  {
+    noteName: "C",
+    hasSharp: true,
+    lineType: "none",
+    octave: 5,
+  },
+  {
+    noteName: "B",
+    hasSharp: false,
+    lineType: "long",
+    octave: 4,
+  },
+  {
+    noteName: "A",
+    hasSharp: true,
+    lineType: "none",
+    octave: 4,
+  },
+  {
+    noteName: "G",
+    hasSharp: true,
+    lineType: "long",
+    octave: 4,
+  },
+  {
+    noteName: "F",
+    hasSharp: true,
+    lineType: "none",
+    octave: 4,
+  },
+  {
+    noteName: "E",
+    hasSharp: false,
+    lineType: "long",
+    octave: 4,
+  },
+  {
+    noteName: "D",
+    hasSharp: true,
+    lineType: "none",
+    octave: 4,
+  },
+  {
+    noteName: "C",
+    hasSharp: true,
+    lineType: "long",
+    octave: 4,
+  },
+  {
+    noteName: "B",
+    hasSharp: false,
+    lineType: "none",
+    octave: 3,
+  },
+  {
+    noteName: "A",
+    hasSharp: true,
+    lineType: "long",
+    octave: 3,
+  },
+  {
+    noteName: "G",
+    hasSharp: true,
+    lineType: "none",
+    octave: 3,
+  },
+  {
+    noteName: "F",
+    hasSharp: true,
+    lineType: "long",
+    octave: 3,
+  },
+  {
+    noteName: "E",
+    hasSharp: false,
+    lineType: "none",
+    octave: 3,
+  },
+  {
+    noteName: "D",
+    hasSharp: true,
+    lineType: "long",
+    octave: 3,
+  },
+  {
+    noteName: "C",
+    hasSharp: true,
+    lineType: "none",
+    octave: 3,
+  },
+  {
+    noteName: "B",
+    hasSharp: false,
+    lineType: "long",
+    octave: 2,
+  },
+  {
+    noteName: "A",
+    hasSharp: true,
+    lineType: "none",
+    octave: 2,
+  },
+  {
+    noteName: "G",
+    hasSharp: true,
+    lineType: "long",
+    octave: 2,
+  },
+  {
+    noteName: "F",
+    hasSharp: true,
+    lineType: "none",
+    octave: 2,
+  },
+  {
+    noteName: "E",
+    hasSharp: false,
+    lineType: "short",
+    octave: 2,
+  },
+  {
+    noteName: "D",
+    hasSharp: true,
+    lineType: "none",
+    octave: 2,
+  },
+  {
+    noteName: "C",
+    hasSharp: true,
+    lineType: "short",
+    octave: 2,
+  },
+  {
+    noteName: "B",
+    hasSharp: false,
+    lineType: "none",
+    octave: 1,
+  },
+  {
+    noteName: "A",
+    hasSharp: true,
+    lineType: "short",
+    octave: 1,
+  },
+  {
+    noteName: "G",
+    hasSharp: true,
+    lineType: "none",
+    octave: 1,
+  },
+  {
+    noteName: "F",
+    hasSharp: false,
+    lineType: "short",
+    octave: 1,
+  },
+];
